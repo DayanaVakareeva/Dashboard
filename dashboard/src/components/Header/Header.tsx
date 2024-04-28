ee267495df0bb8d568cbc4020f01a410daab953e
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-04-28 03:56:03 +0300

    add header

---
+++ dashboard/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+
+type HeaderProps = {
+  title: string;
+};
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
+  return (
+    <header className="p-1 h-16 border-b-2 border-white flex justify-center items-center w-1/2 mx-auto overflow-hidden">
+      
+    </header>
+  );
+};
+
+export default Header;
