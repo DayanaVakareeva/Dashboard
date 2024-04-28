4a3bc664fb154d92650265ee4c8d4f90444ec79e
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-04-28 03:56:21 +0300

    add login and data

---
+++ dashboard/src/constants/data.tsx
@@ -0,0 +1,61 @@
+{
+    "success": true,
+    "payload": [
+        {
+            "id": 1,
+            "product": "Water bottles",
+            "quantity": 5, 
+            "priceForOne": 10,
+            "timestamp": 1713974040
+        },
+        {
+            "id": 2,
+            "product": "Milk bottles",
+            "quantity": 2, 
+            "priceForOne": 105,
+            "timestamp": 1713974040
+        },
+        {
+            "id": 3,
+            "product": "Phone Cases",
+            "quantity": 5, 
+            "priceForOne": 101,
+            "timestamp": 1713974040
+        },
+        {
+            "id": 4,
+            "product": "Cables",
+            "quantity": 5, 
+            "priceForOne": 2,
+            "timestamp": 1713974040
+        },
+                {
+            "id": 5,
+            "product": "Glasses 1",
+            "quantity": 5, 
+            "priceForOne": 19,
+            "timestamp": 1713974040
+        },
+        {
+            "id": 6,
+            "product": "Glasses 2",
+            "quantity": 5, 
+            "priceForOne": 27,
+            "timestamp": 1713974040
+        },
+        {
+            "id": 7,
+            "product": "Headphones",
+            "quantity": 5, 
+            "priceForOne": 16,
+            "timestamp": 1713974040
+        },
+        {
+            "id": 8,
+            "product": "Wallets",
+            "quantity": 5, 
+            "priceForOne": 40,
+            "timestamp": 1713974040
+        }
+    ]
+}
