88105ce276d3fecbc358a11d2e944a46f57a4de8
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-04-28 03:55:45 +0300

    cofig tailwind

---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+  },
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#020024',
+        'brand-orange': '#FF6900',
+      }
+    },
+  },
+  plugins: [],
+}
