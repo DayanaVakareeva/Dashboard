3525faeec286038d9ea6cecef5e5dc6856de10e8
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-05-07 04:16:09 +0300

    App adds

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,85 +1,120 @@
-import { useState } from 'react';
-import './App.css';
+import React, { useState, useCallback, useEffect } from 'react';
 import Header from './components/Header/Header';
 import Login from './views/Login/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useLocation, } from 'react-router-dom';
 import Sidebar from './components/SideBar/Sidebar';
-import Order from './components/Order/Order';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { useEffect } from 'react';
 import Footer from './components/Footer/Footer';
 import OrderHistory from './views/OrderHistory/OrderHistory';
 import AppContext from './context/AppContext';
 import Dashboard from './views/MainDashboard/MainDashboard';
 import { Toaster } from 'react-hot-toast';
 import SignIn from './components/SignIn/SignIn';
+import pieChart1 from './constants/pieChart1.jpg';
+import Authenticated from './hoc/Authenticated';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+/**
+ * AppContent component
+ *
+ * This component renders the main content of the application.
+ *
+ * Returns:
+ * - The main content of the application, which can include various components and views based on the application's routing and state.
+ */
+const AppContent = React.memo(function AppContent() {
+  console.log('AppContent rendered');
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+  const isSignInPage = location.pathname === '/sign-in';
+
+  const backgroundImage = isLoginPage || isSignInPage ? `url(${pieChart1})` : 'none';
+
+  return (
+    <div
+      className="font-rubik bg-dark-blue flex flex-col justify-between text-salmon h-screen"
+      style={{ backgroundImage, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}
+    >
+      <div className="flex-grow flex overflow-auto">
+        <Sidebar />
+        <div className="flex-grow">
+          <Header style={{ height: '50px' }} />
+          <Routes>
+            <Route
+              path="/dashboard"
+              element={
+                <Authenticated>
+                  <Dashboard />
+                </Authenticated>
+              }
+            />
+            <Route
+              path="/order-history"
+              element={
+                <Authenticated>
+                  <OrderHistory />
+                </Authenticated>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+});
 
 /**
  * App component
  *
- * This is the root component of the application. It handles user authentication and routing.
+ * This is the root component of the application. It sets up the context provider and routing.
  *
  * State:
  * - appState: { user: User | null, userData: any | null } - The current user and their data.
- * - isOrderFormOpen: boolean - Whether the order form is currently open.
  *
  * Context:
  * - AppContext: { user: User | null, userData: any | null } - Provides the current user and their data to the rest of the application.
  *
- * Effects:
- * - On mount, subscribes to auth state changes. If a user is signed in, it updates appState.user with the user. If a user is signed out, it updates appState.user to null. Cleans up the subscription on unmount.
- *
  * Returns:
- * - If a user is signed in, returns a layout with a sidebar, header, order form, and routes for the dashboard and order history.
- * - If no user is signed in, returns the Login view.
+ * - A context provider that provides the current user and their data to the rest of the application.
+ * - A router that handles routing for the application.
+ * - The `AppContent` component, which renders the rest of the application.
  */
 function App() {
-  const [appState, setAppState] = useState({
-    user: null,
-    userData: null,
-  });
-
-  const [isOrderFormOpen, setOrderFormOpen] = useState(false);
+  console.log('App rendered');
+  const [appState, setAppState] = useState({user: null, userData: null, loading: true});
 
   useEffect(() => {
     const auth = getAuth();
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, update appState.user with the user.
-        setAppState((prevState) => ({ ...prevState, user }));
-      } else {
-        // User is signed out, update appState.user to null.
-        setAppState((prevState) => ({ ...prevState, user: null }));
-      }
+      setAppState(prevState => ({ ...prevState, user, loading: false }));
     });
 
-    // Cleanup subscription on unmount
-    return () => unsubscribe();
+    return () => {
+      unsubscribe();
+    };
   }, []);
+
+  const memoizedSetAppState = useCallback((newState) => {
+    setAppState(prevState => ({ ...prevState, ...newState }));
+  }, []);
+
+  if (appState.loading) {
+    return <div>Loading...</div>; // Or your custom loading component
+  }
 
   return (
     <div>
       <Toaster />
-      <AppContext.Provider value={{...appState, setAppState}}>
+      <AppContext.Provider value={{ ...appState, setAppState: memoizedSetAppState }}>
         <Router>
-          <div className="font-rubik bg-dark-blue flex flex-col justify-between text-orange-500">
-            <div className="h-screen flex overflow-auto">
-              <Sidebar />
-              <div className="flex-grow">
-                <Header style={{ height: '50px' }} />
-                <Routes>
-                  <Route path="/dashboard" Component={Dashboard} />
-                  <Route path="/order-history" Component={OrderHistory} />
-                  <Route path="/login" Component={Login} />
-                  <Route path="/sign-in" Component={SignIn} />
-                </Routes>
-              </div>
-            </div>
-            <Footer />
-          </div>
+          <AppContent />
         </Router>
       </AppContext.Provider>
     </div>
   );
-};
+}
 export default App;
--- dashboard/src/context/AppContext.tsx
+++ dashboard/src/context/AppContext.tsx
@@ -1,14 +1,27 @@
 import { createContext } from "react";
 
+/**
+ * Interface for the context state
+ */
 interface IContext {
+  /** User object or null */
   user: any | null;
+  /** User data object or null */
   userData: any | null;
+  /** Loading state */
+  loading: boolean;
+  /** Function to set the app state */
   setAppState: (state: any) => void;
 }
 
+/**
+ * AppContext is the context object for the app. It provides a user object, user data,
+ * a loading state, and a function to set the app state.
+ */
 const AppContext = createContext<IContext>({
   user: null,
   userData: null,
+  loading: true,
   setAppState: () => {}, 
 });
 
