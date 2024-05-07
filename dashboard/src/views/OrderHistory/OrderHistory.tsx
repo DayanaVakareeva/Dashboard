3bef83258ac4b6d14a6e5a018a95c5e063e3ca59
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-05-07 04:16:42 +0300

    OrderHistory

--- dashboard/src/views/OrderHistory/OrderHistory.tsx
+++ dashboard/src/views/OrderHistory/OrderHistory.tsx
@@ -2,12 +2,19 @@ import OrderList from "../../components/OrderList/OrderList"
 import AppContext from "../../context/AppContext"
 import { useContext } from 'react';
 
+/**
+ * OrderHistory component
+ * @returns JSX.Element
+ */
 export default function OrderHistory() {
+    // Get the app state from the context
     const appState = useContext(AppContext);
+    // Get the user ID from the app state
     const userId = appState.user?.uid;
 
     return (
         <div className="p-10 ">
+        {/* If the user ID exists, render the OrderList component with the user ID as a prop */}
         {userId && <OrderList userId={userId} />}
         </div>
     )
